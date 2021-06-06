7df25f227a78d5dc4ccfec092835524e5b5a8b86
danita240697 <deysedayane.o.c@gmail.com>
2021-06-06 16:47:32 -0300

    Stack exec: Decimal to Binary and .gitignore

--- src/com/datastructure/exec/DecimalToBinary.java
+++ src/com/datastructure/exec/DecimalToBinary.java
@@ -1,10 +1,54 @@
-
 package com.datastructure.exec;
+
+import com.datastructure.stack.Stack;
 
 /**
  *
  * @author deecarneiro
  */
 public class DecimalToBinary {
-    
+
+    public static void main(String[] args) throws Exception {
+
+        printResult(2);
+
+        printResult(4);
+
+        printResult(10);
+
+        printResult(25);
+
+        printResult(100);
+
+    }
+
+    public static void printResult(int number) throws Exception {
+
+        System.out.println(number + " in binary system is: " + decimalToBinary(number));
+    }
+
+    public static String decimalToBinary(int number) throws Exception {
+
+        Stack<Integer> stack = new Stack<>();
+
+        String numBinary = "";
+
+        int remainder;
+
+        while (number > 0) {
+            remainder = number % 2;
+
+            stack.push(remainder);
+
+            number /= 2;
+
+        }
+
+        while (!stack.isEmpty()) {
+
+            numBinary += stack.pop();
+        }
+
+        return numBinary;
+    }
 }
